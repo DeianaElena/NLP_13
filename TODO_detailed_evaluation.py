2f3e943b04444accb1852bed124f1b7208061c30
ozogari <39986270+ozogari@users.noreply.github.com>
2022-04-22 22:12:21 +0200

    Update TODO_detailed_evaluation.py

--- TODO_detailed_evaluation.py
+++ TODO_detailed_evaluation.py
@@ -5,11 +5,13 @@ import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
+import pandas as pd
 import re
 
 def process_output(test_input):
     predictions = []
     instances = []
+    
     
     for instance in test_input:
         instance = instance.replace("\n","")
@@ -34,7 +36,7 @@ def process_output(test_input):
 
 
 if __name__ == '__main__':
-    path = "/experiments/base_model/"
+    path = "/Users/ozogari/Ouail Zogari/School/VU/Master/AI (jaar 2)/Periode 5/NLP Technology/Group Assignments/NLP_13-main/experiments/base_model/"
 
     # Note: this loads all instances into memory. If you work with bigger files in the future, use an iterator instead.
 
@@ -42,4 +44,15 @@ if __name__ == '__main__':
         model_output = sent_file.readlines()
 
     cm = process_output(model_output)
-    print(cm)   
+    print(cm)
+
+
+
+    data = {
+        'epoch': [1,5,10,15,25, 40,50],
+        'F1 weighted average': [0.70, 0.82,0.84,0.84,0.84,0.84,0.84],
+        }
+
+    df = pd.DataFrame(data)
+    print(df)
+    df.plot.line()
